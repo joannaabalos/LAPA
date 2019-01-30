19c4ce0ab159ce3b7d6984c32fc8fd68a12a1e5f
joannaabalos <46577250+joannaabalos@users.noreply.github.com>
2019-01-30 17:46:57 -0500

    corrected

--- LAPA2.m
+++ LAPA2.m
@@ -4,8 +4,9 @@ clc
 maxX = 10;
 maxY = 10;
 V = zeros(maxX,maxY);
+V2 = zeros(maxX,maxY);
 iter = 0;
-maxiter = 100000;
+maxiter = 1000;
 
 while iter < maxiter
     V(1,:) = 1;
@@ -14,26 +15,33 @@ while iter < maxiter
     V(:,maxY) = 0;
 
 
+
     for m = 2:maxX-1
         for n = 2:maxY-1
                 V(m,n) = (V(m+1,n)+V(m-1,n)+V(m,n+1)+V(m,n-1)) * 1/4; %iterate all nodes
         end
     end
-%     figure (1)
-%     E = -1*gradient(V);
-%     surf(V.')
+    figure (1)
+    [Ex, Ey] = gradient(V);
+    surf(V.')
     
-%     figure (2)
-%     quiver(V,E)
+    figure (2)
+    quiver(-Ex,-Ey)
 
+
+
+    V2(1,:) = 1;
+    V2(maxX,:) = 1;
+    V2(:,1) = 0;
+    V2(:,maxY) = 0;
+    
     %TAKE 2
     figure(3)
-    take2 = imboxfilt(V);
-    surf(take2)
-
+    V2 = imboxfilt(V2);
+    surf(V2)
 
     iter = iter + 1;
+    pause(0.1)
 end
-
 
 
